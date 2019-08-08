2c17415be699c2731c887efb701bfa9768986315
hilotter <hilotter3@gmail.com>
2019-08-08 19:23:19 +0900

    add api

--- README.md
+++ README.md
@@ -6,9 +6,48 @@
  * `cdk diff`        compare deployed stack with current state
  * `cdk synth`       emits the synthesized CloudFormation template
 
-# custom settings
+# Custom settings
 
 ```
 export BOUNCE_RECORDER_SNS_TOPIC=ses-notifications
 export BOUNCE_RECORDER_DYNAMODB_TABLE=ses-mailing
+export BOUNCE_RECORDER_SCAN_LIMIT=1000
+```
+
+# Add SNS topic to SES notification setting
+
+![notification](https://user-images.githubusercontent.com/1042519/62696253-ff7bec80-ba12-11e9-9962-4f55d37901f4.png)
+
+# Usage
+
+- Get bounce data (default: 1000 items/req)
+
+```
+curl --header 'x-api-key:{api-key}' -i 'https://{id}.execute-api.us-east-1.amazonaws.com/prod/'
+
+{
+  "Items": [
+    {
+      // ...
+    },
+    {
+      "notificationType": "Bounce",
+      "UserId": "bounce@simulator.amazonses.com",
+      "from": "test@example.com",
+      "state": "disable",
+      "timestamp": "2019-08-08T09:48:18.000Z"
+    }
+  ],
+  "Count": 1000,
+  "ScannedCount": 1000,
+  "LastEvaluatedKey": {
+    "UserId": "bounce@simulator.amazonses.com"
+  }
+}
+```
+
+- Get next items by add `lastUserId` parameter
+
+```
+curl --header 'x-api-key:{api-key}' -i 'https://{id}.execute-api.us-east-1.amazonaws.com/prod/?lastUserId=bounce@simulator.amazonses.com'
 ```
--- lib/bounce-recorder-stack.ts
+++ lib/bounce-recorder-stack.ts
@@ -3,6 +3,9 @@ import sns = require('@aws-cdk/aws-sns');
 import subs = require('@aws-cdk/aws-sns-subscriptions');
 import dynamodb = require('@aws-cdk/aws-dynamodb')
 import lambda = require('@aws-cdk/aws-lambda')
+import apigw = require('@aws-cdk/aws-apigateway');
+import { ApiKeySourceType } from '@aws-cdk/aws-apigateway';
+import { read } from 'fs';
 
 export class BounceRecorderStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -18,7 +21,7 @@ export class BounceRecorderStack extends cdk.Stack {
       partitionKey: { name: 'UserId', type: dynamodb.AttributeType.STRING }
     });
 
-    const handler = new lambda.Function(this, 'BounceRecorderHandler', {
+    const recordHandler = new lambda.Function(this, 'BounceRecorderHandler', {
       runtime: lambda.Runtime.NODEJS_10_X,
       handler: 'bounce_recorder.handler',
       code: lambda.Code.asset('lambda'),
@@ -28,7 +31,37 @@ export class BounceRecorderStack extends cdk.Stack {
       }
     });
 
-    table.grantReadWriteData(handler);
-    topic.addSubscription(new subs.LambdaSubscription(handler));
+    table.grantReadWriteData(recordHandler);
+    topic.addSubscription(new subs.LambdaSubscription(recordHandler));
+
+    const scanLimit = process.env.BOUNCE_RECORDER_SCAN_LIMIT || '1000';
+    const readHandler = new lambda.Function(this, 'BounceReaderHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: 'bounce_reader.handler',
+      code: lambda.Code.asset('lambda'),
+      timeout: cdk.Duration.seconds(300),
+      environment: {
+        DYNAMODB_TABLE_NAME: table.tableName,
+        DYNAMODB_SCAN_LIMIT: scanLimit
+      }
+    });
+
+    table.grantReadData(readHandler);
+
+    const api = new apigw.RestApi(this, 'BounceReaderEndpoint', {});
+    const integration = new apigw.LambdaIntegration(readHandler);
+    const echo = api.root.addMethod('GET', integration, {
+      apiKeyRequired: true
+    })
+    const apiKey = api.addApiKey('BounceReaderApiKey');
+    const plan = api.addUsagePlan('UsagePlan', {
+      name: 'BounceReader',
+      apiKey: apiKey
+    });
+
+    plan.addApiStage({
+      stage: api.deploymentStage,
+      throttle: []
+    });
   }
 }
