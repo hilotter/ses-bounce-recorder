9e94271a107d08ae9c82a1eb9368c62d518e21f8
hilotter <hilotter3@gmail.com>
2022-07-16 20:30:47 +0900

    fix eslint setting & fix error handling

--- src/lambda/handlers/sesBounceRecorder.ts
+++ src/lambda/handlers/sesBounceRecorder.ts
@@ -105,17 +105,21 @@ const sendWebhook = async (address: string, notificationType: string) => {
     )
   } catch (error) {
     // https://github.com/axios/axios#handling-errors
-    if (error.response) {
-      console.log(error.response.data)
-      console.log(error.response.status)
-      console.log(error.response.headers)
-    } else if (error.request) {
-      console.log(error.request)
-    } else {
-      console.log('Error', error.message)
+    if (axios.isAxiosError(error)) {
+      if (error.response) {
+        console.log(error.response.data)
+        console.log(error.response.status)
+        console.log(error.response.headers)
+      } else if (error.request) {
+        console.log(error.request)
+      } else {
+        console.log('Error', error.message)
+      }
+      console.log(error.config)
+      console.log(error.message)
+      return
     }
-    console.log(error.config)
-    console.log(error.message)
+    throw error
   }
 }
 
