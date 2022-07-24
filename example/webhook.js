fe3fa5b274ea18ae162f2f5737a803bdf855b0c6
hilotter <hilotter3@gmail.com>
2022-07-24 16:47:26 +0900

    add cdk test

--- example/webhook.js
+++ example/webhook.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const express = require('express')
 const app = express()
 
---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  snapshotSerializers: ['<rootDir>/test/plugin/snapshot-plugin.ts'],
+}
